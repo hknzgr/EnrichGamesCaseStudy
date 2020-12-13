c1086cf8f3436e84b28fde3983e1043e3b98be42
Hakan ÖZGER <35780405+hknzgr@users.noreply.github.com>
2020-12-13 20:32:53 +0300

    v1.0

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,21 +5,38 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    public int n;
+    public static int n;
+    public Text nText;
     public GameObject slot;
-
+    public GameObject grid;
+    List<GameObject> gridIndex = new List<GameObject>();
 
     private void Start()
     {
-        CreateGrid();
+        n = int.Parse(nText.text);
     }
+
     public void CreateGrid()
     {
+        n = int.Parse(nText.text);
+
+        ClearGrid();
+
         for (int i = 0; i < n*n; i++)
         {
-            GameObject slotN = Instantiate(slot, GameObject.Find("GridPanel").transform.position, Quaternion.identity);
-            slotN.transform.SetParent(GameObject.Find("GridPanel").transform);
+            GameObject slotN = Instantiate(slot, grid.transform.position, Quaternion.identity);
+            slotN.transform.SetParent(grid.transform);
+            gridIndex.Add(slotN);
             slotN.name = (i+1).ToString();
+        }
+
+    }
+
+    public void ClearGrid()
+    {
+        foreach (var item in gridIndex)
+        {
+            Destroy(item.gameObject);
         }
     }
 
---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    public string Name;
+    public string Position;
+    public bool Clicked;
+    public List<GameObject> Targets = new List<GameObject>();
+    int n,x;
+
+    void Start()
+    {
+        Invoke("Properties", 1);
+        
+    }
+
+    public void ButtonCliked()
+    {
+        Clicked = true;
+    }
+
+    public void Properties()
+    {
+
+        n = GameController.n;
+        x = int.Parse(this.gameObject.name);
+        Name = this.gameObject.name;
+        if (Name == "1")
+        {
+            Position = "UpperLeftCorner";
+            Targets.Add(GameObject.Find((x + 1).ToString()));
+            Targets.Add(GameObject.Find((x + n).ToString()));
+
+        }
+        else if (Name == n.ToString())
+        {
+            Position = "UpperRightCorner";
+            Targets.Add(GameObject.Find((x - 1).ToString()));
+            Targets.Add(GameObject.Find((x + n).ToString()));
+        }
+        else if (Name == ((n * n) - n + 1).ToString())
+        {
+            Position = "LowerLeftCorner";
+            Targets.Add(GameObject.Find((x - n).ToString()));
+            Targets.Add(GameObject.Find((x + 1).ToString()));
+        }
+        else if (Name == (n * n).ToString())
+        {
+            Position = "LowerRightCorner";
+            Targets.Add(GameObject.Find((x - n).ToString()));
+            Targets.Add(GameObject.Find((x - 1).ToString()));
+        }
+        else if (int.Parse(Name) < n && int.Parse(Name) > 1)
+        {
+            Position = "Upper";
+            Targets.Add(GameObject.Find((x - 1).ToString()));
+            Targets.Add(GameObject.Find((x + 1).ToString()));
+            Targets.Add(GameObject.Find((x + n).ToString()));
+        }
+        else if (int.Parse(Name) < n * n && int.Parse(Name) > (n * n) - n + 1)
+        {
+            Position = "Lower";
+            Targets.Add(GameObject.Find((x - 1).ToString()));
+            Targets.Add(GameObject.Find((x + 1).ToString()));
+            Targets.Add(GameObject.Find((x - n).ToString()));
+        }
+        else if (int.Parse(Name) % n == 1 && Position == "")
+        {
+            Position = "Left";
+            Targets.Add(GameObject.Find((x + 1).ToString()));
+            Targets.Add(GameObject.Find((x - n).ToString()));
+            Targets.Add(GameObject.Find((x + n).ToString()));
+        }
+        else if (int.Parse(Name) % n == 0 && Position == "")
+        {
+            Position = "Right";
+            Targets.Add(GameObject.Find((x - 1).ToString()));
+            Targets.Add(GameObject.Find((x - n).ToString()));
+            Targets.Add(GameObject.Find((x + n).ToString()));
+        }
+        else
+        {
+            Position = "Middle";
+            Targets.Add(GameObject.Find((x - 1).ToString()));
+            Targets.Add(GameObject.Find((x + 1).ToString()));
+            Targets.Add(GameObject.Find((x - n).ToString()));
+            Targets.Add(GameObject.Find((x + n).ToString()));
+        }
+    }
+}
